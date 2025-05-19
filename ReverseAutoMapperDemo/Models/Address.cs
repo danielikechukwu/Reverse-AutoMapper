5d0aceb9af7ef1bd92476c348c74c2f07dd0bdcc
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-20 00:16:15 +0100

    Add project files.

---
+++ ReverseAutoMapperDemo/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿namespace ReverseAutoMapperDemo.Models
+{
+    public class Address
+    {
+        public int Id { get; set; } // Unique identifier for the address
+
+        public string City { get; set; } // City of the address
+
+        public string State { get; set; } // State of the address
+
+        public string Country { get; set; } // Country of the address
+
+        public int EmployeeId { get; set; } // Foreign key to Employee
+
+        public Employee Employee { get; set; } // Navigation property for Employee
+
+    }
+}
---
+++ ReverseAutoMapperDemo/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace ReverseAutoMapperDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } // Primary key for the employee
+
+        public string FirstName { get; set; } // Employee's first name
+
+        public string LastName { get; set; } // Employee's last name
+
+        public string Email { get; set; } // Employee's email address
+
+        public string Gender { get; set; } // Employee's gender
+
+        public Address Address { get; set; } // Navigation property to the Address entity
+
+    }
+}
